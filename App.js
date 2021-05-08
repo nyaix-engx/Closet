d0126784ae05564646a696a13e6a83fed5b76585
Sanjay <engnx@Sanjays-MacBook-Air.local>
2021-05-08 21:00:02 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import LottieView from 'lottie-react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.backgroundStyle}>
+      <LottieView source={require('./notifications.json')} autoPlay loop style={{width:100,height:100}} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Closet
+//
+//  Created by Sanjay Sajwan on 08/05/21.
+//
+
+import Foundation
